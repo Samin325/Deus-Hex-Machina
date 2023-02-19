043791e16a5c9cf0aec4a1b69ef0389632765868
tmannan <62124462+roljy@users.noreply.github.com>
2023-02-18 17:02:33 -0700

    Create coord conversion methods and docstrings

--- src/coord.py
+++ src/coord.py
@@ -1,7 +1,14 @@
 # coord.py
 
+
 class Coord:
     def __init__(self, x: int, y: int) -> None:
+        """ Create a Coord object
+
+        Parameters:
+            x: (int) position across. converted to letter
+            y: (int) position downwards. remains as number
+        """
         # may need to revise parameters later
         self.__x = x
         self.__y = y
@@ -12,7 +19,7 @@ class Coord:
 
     def getx(self) -> int:
         return self.__x
-    
+
     def gety(self) -> int:
         return self.__y
 
@@ -24,11 +31,35 @@ class Coord:
 
     @staticmethod
     def cart2str(x: int, y: int) -> str:
-        # TODO: finish
-        return "a1"
+        """ Convert a cartesian pair to a chess-style string
+
+        Parameters:
+            x: (int) position across. converted to letter
+            y: (int) position downwards. remains as number
+
+        Returns: (str)
+            chess-style coordinate eg. "b5"
+        """
+        if x <= 0:
+            letter = '-'
+        elif x > 26:
+            letter = '+'
+        else:
+            letter = chr(ord('a') + x - 1)
+        if y < 0:
+            y = 0
+        return letter + str(y)
 
     @staticmethod
     def str2cart(name: str) -> tuple:
-        # TODO: finish
-        # may not need this one at all
-        return (1, 1)
+        """ Convert a chess-style string to a cartesian pair
+
+        Parameters:
+            name: (str) chess-style string eg. "b5"
+
+        Returns: (tuple[int, int])
+            equivalent x and y coordinates
+        """
+        x = ord(name[0]) - ord('a') + 1
+        y = int(name[1:])
+        return (x, y)
