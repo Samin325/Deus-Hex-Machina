8cd03b8019f3814eca7dd6c7f4cb3b85e63b5bcf
tmannan <62124462+roljy@users.noreply.github.com>
2023-02-18 22:40:26 -0700

    Modify constants.py to organize into enums

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,18 @@
-# These can be any numbers we want
-EMPTY = 10
-WHITE = 3
-BLACK = 4
+# constants.py
+
+from enum import Enum
+
+
+class Color(Enum):
+    EMPTY = 0
+    WHITE = 1
+    BLACK = -1
+
+
+class Status(Enum):
+    READY = 0       # origin empty
+    HALFWAY = 0.5   # origin+depend friendly, dest not hostile
+    TO_BE = 1       # origin friendly,  depends+dest empty
+    SUCCESS = 2     # origin+dest friendly, depends empty
+    FAIL = -1       # dest hostile / depends both hostile / dest empty+depend hostile
+    JEOPARDY = 4    # origin+dest friendly, 1depend hostile
